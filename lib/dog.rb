438ffac0273f9c65a26d34c07690b256e7fbab28
Charles Butler <cdbutle@outlook.com>
2020-03-15 23:24:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Dog
+  def name= (dog_name)
+    @dogs_name_is = dog_name
+  end
+  
+  def dog_name
+    @dogs_name_is
+  end
+  def bark
+    puts "woof!"
+  end
+end
